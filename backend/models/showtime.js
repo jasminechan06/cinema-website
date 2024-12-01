443a7320eefe0165271fefece1c62fcaa9d4c09c
Kouroshe84 <kouroshe84@gmail.com>
2024-11-30 23:01:37 -0500

    Updating the Showtime db model

--- backend/models/showtime.js
+++ backend/models/showtime.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const showtimeSchema = new mongoose.Schema({
   movie: { type: mongoose.Schema.Types.ObjectId, ref: "Movie", required: true },
+  theater: {type: String, required: true},
+  screen: {type: Number, required: true}, 
   date: { type: Date, required: true },
   time: { type: String, required: true },
   availableSeats: { type: Number, required: true },
